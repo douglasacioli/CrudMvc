52d0beb44d54de1011c120f2e23a8ff875397f74
Douglas Acioli <douglas.acioli@gmail.com>
2021-03-09 00:22:59 -0300

    Criando DbContext

---
+++ CrudMvc/Models/EscolaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CrudMvc.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext() : base("Escola")
+        {
+            Database.CreateIfNotExists(); 
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+   
+    }
+}
